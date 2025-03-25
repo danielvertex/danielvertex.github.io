e25f8a8cfe55bd2782c0e57f0169ece51f9f2d9e
danielvertex <ag.herreradaniel@gmail.com>
2025-03-25 02:47:28 +0000

    Deploying to gh-pages from @ danielvertex/danielvertex.github.io@0c17232b77b3b0bbf4c830dbfb014107b2c03ab3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -93,11 +93,18 @@ ninja.data = [{
           section: "Projects",handler: () => {
               window.location.href = "/projects/TICKER%20INFORMATION/";
             },},{
+        id: 'social-bluesky',
+        title: 'Bluesky',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://bsky.app/profile/danielvertex.bsky.social", "_blank");
+        },
+      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%67.%68%65%72%72%65%72%61%64%61%6E%69%65%6C@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
         id: 'social-inspire',
@@ -107,18 +114,18 @@ ninja.data = [{
           window.open("https://inspirehep.net/authors/1010907", "_blank");
         },
       },{
+        id: 'social-linkedin',
+        title: 'LinkedIn',
+        section: 'Socials',
+        handler: () => {
+          window.open("https://www.linkedin.com/in/daniel-herrera-aguirre", "_blank");
+        },
+      },{
         id: 'social-rss',
         title: 'RSS Feed',
         section: 'Socials',
         handler: () => {
           window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
